7b7c8c5858f4729a91999e5d103b8dee7081aa19
LiGuiye <1171116470@qq.com>
2025-03-12 18:17:25 -0600

    remove unused tiff options and simplify the code

--- nets/dataset.py
+++ nets/dataset.py
@@ -1,36 +1,23 @@
-from glob import glob
-
 import numpy as np
-from PIL import Image
-from torch.utils.data import Dataset
-from torchvision import transforms
+from glob import glob
 from utils.utils import normalize
+from torchvision import transforms
+from torch.utils.data import Dataset
 
 
 class get_dataset(Dataset):
-    def __init__(self, path, image_size, entire_mean, entire_std, image_type):
-        """
-        Args:
-            image_type: 'Solar', 'Wind'
-        """
+    def __init__(self, path, image_size, entire_mean, entire_std):
         self.root = path
-        self.type = image_type
         self.entire_mean = entire_mean
         self.entire_std = entire_std
 
         self.files = []
-        if type(path) == list:
-            for i in path:
-                self.files += glob(i + '/*.npy')
-            assert self.files is not None, 'No data found.'
-            self.suffix = 'npy'
+        if type(self.root) == list:
+            for i in self.root:
+                self.files += glob(f"{i}/*.npy")
         else:
-            self.files = glob(self.root + '/*.tif')
-            self.suffix = 'tif'
-            if not self.files:  # in case images are in npy format
-                self.files = glob(self.root + '/*.npy')
-                assert self.files is not None, 'No data found.'
-                self.suffix = 'npy'
+            self.files = glob(f"{self.root}/*.npy")
+        assert len(self.files), 'No data found.'
 
         self.resize2tensor = transforms.Compose(
             [
@@ -50,7 +37,7 @@ class get_dataset(Dataset):
 
     def __getitem__(self, index):
         if isinstance(index, str):
-            f = glob(self.root + '/' + index + '.' + self.suffix)[0]
+            f = glob(f"{self.root}/{index}.npy")[0]
         else:
             try:
                 f = self.files[index]
@@ -58,10 +45,7 @@ class get_dataset(Dataset):
                 print("Given index doesn't exist, the first is used.")
                 f = self.files[0]
 
-        if self.suffix == 'tif':
-            img = Image.open(f).astype(np.float32)
-        else:
-            img = np.load(f).astype(np.float32)
+        img = np.load(f).astype(np.float32)
 
         img = self.resize2tensor(img)
         img = normalize(img, self.entire_mean, self.entire_std)
